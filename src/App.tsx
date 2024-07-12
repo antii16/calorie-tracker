4709b11bf82f00d2f6d5af63910244ed50b3a081
Antonella <98277771+antii16@users.noreply.github.com>
2024-07-12 21:22:21 +0200

    localStorage para almacenar actividades

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useReducer, useEffect } from "react"
 import { activityReducer, initialState } from "./reducers/activity-reducer"
-import { useReducer } from "react"
 import Form from "./components/Form"
 import ActivityList from "./components/ActivityList"
 
 function App() {
   const[state, dispatch] = useReducer(activityReducer, initialState)
   
+  useEffect(()=>{
+    localStorage.setItem('activities', JSON.stringify(state.activities))
+  }, [state.activities])
 
   return (
     <>
