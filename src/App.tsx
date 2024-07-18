880d0216f0fb1edcd7445609254d741a4aa0b249
Antonella <98277771+antii16@users.noreply.github.com>
2024-07-18 22:10:50 +0200

    Crear componente para cantidades

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { useReducer, useEffect } from "react"
+import { useReducer, useEffect, useMemo} from "react"
 import { activityReducer, initialState } from "./reducers/activity-reducer"
 import Form from "./components/Form"
 import ActivityList from "./components/ActivityList"
+import CalorieTracker from "./components/CalorieTracker"
+
 
 function App() {
   const[state, dispatch] = useReducer(activityReducer, initialState)
@@ -10,12 +12,23 @@ function App() {
     localStorage.setItem('activities', JSON.stringify(state.activities))
   }, [state.activities])
 
+  // Si el usuario añade actividades, se utiliza esta función
+  const canRestartApp = ()=> useMemo(()=> state.activities.length, [state.activities])
+
   return (
     <>
       <header className='bg-lime-600 py-3'>
-        <div className="max-w-4xl mx-auto flex justify-between">
+        <div className="max-w-4xl mx-auto flex justify-between items-center">
           <h1 className="text-center text-lg font-bold text-white uppercase">Contador de calorías</h1>
+          <button  className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer
+          rounded-lg text-sm disabled:opacity-10"
+          disabled = {!canRestartApp()}
+          onClick={()=>dispatch({type: 'restart-app'})}
+          >
+            Reiniciar App
+          </button>
         </div>
+        </header>
         <section className="bg-lime-500 py-20 px-5">
           <div className="max-w-4xl mx-auto">
             <Form 
@@ -24,13 +37,21 @@ function App() {
             />
           </div>
         </section>
+        <section className="bg-gray-800 py-20">
+          <div className="max-w-4xl mx-auto">
+            <CalorieTracker 
+              activities={state.activities}
+            />
+          </div>
+
+        </section>
         <section className="p-10 mx-auto max-w-4xl bg-white">
           <ActivityList
             activities = {state.activities}
             dispatch = {dispatch}
           />
         </section>
-      </header>
+      
     </>
   )
 }
